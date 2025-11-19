4bb32177a40eaae77057e8c2f530e2028a29bb68
harshyadav10134-lab <harshyadav10134@gmail.com>
2025-11-19 11:49:28 +0000

    week8

---
+++ week8/index6.js
@@ -0,0 +1,27 @@
+mongoose = require('mongoose');
+//app = express();
+const MONGO_URI = 'mongodb+srv://harshyadav10134_db_user:VOGJrdSNjNx5PotY@cluster0.ztg0pvs.mongodb.net/week8';
+mongoose.connect(MONGO_URI, {useUnifiedTopology: true,useNewUrlParser: true});
+const db = mongoose.connection;
+db.on('error', function(err)
+    {
+        console.log("Error occured during connection"+err)
+    });
+db.once('connected', function() {
+console.log(`Connected to ${MONGO_URI}`);
+});
+// creating the scheme
+const PersonScheme = new mongoose.Schema({ name: {type: String, required: true},age: Number, Gender:String, Salary:Number});
+// creating model named as modelname with collection named as personCollection
+const person_doc = mongoose.model('modelname', PersonScheme,'personCollection');
+// creating a single document
+const doc1 = new person_doc({ name: 'Jacky',age:362,Gender:"Male",Salary:3456 }); 
+// adding one document in the collection
+doc1
+    .save()
+    .then((doc1) => {
+        console.log("New Article Has been Added Into Your DataBase.",doc1);
+    })
+    .catch((err) => {
+        console.error(err);
+    });
